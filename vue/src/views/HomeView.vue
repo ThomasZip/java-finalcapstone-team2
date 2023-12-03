d1ba6ccc031699b1337f915c16a9a8bc3a9d0aab
Tom Saunders <tomsaundersclev@gmail.com>
2023-12-03 13:35:59 -0500

    Jody's changes

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,10 +1,13 @@
+HomeView.vue (copy and paste)
 <template>
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
   </div>
+  <nav>
+    <router-link v-bind:to="{name: 'restaurantsSearch'}">Search for Restaurants</router-link>
+  </nav>
 </template>
-
 <script>
 export default {
 };
