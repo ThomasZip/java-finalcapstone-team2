03cbdc47ac9e991834ff06909868eb04cfc5f64f
Jody Nguyen <baoanjnguyen@gmail.com>
2023-12-05 10:20:45 -0500

    updated RestaurantSearchView and methods with functional Go button to next page

--- vue/src/views/RestaurantSearchView.vue
+++ vue/src/views/RestaurantSearchView.vue
@@ -1,6 +1,7 @@
 <template>
     <h1>Search for Restaurants</h1>
-    <form v-on:submit.prevent="addZipcode" class="zipcode-form">
+    <!--v-on:submit.prevent="addZipcode"-->
+    <form class="zipcode-form" v-on:submit.prevent="onSubmit">
         <div class="field">
             <label for="zipcodeInput">Enter a Zipcode: </label>
             <input id="zipcodeInput" name="zipcodeInput" type="text" v-model="zipcodeInput"/>
@@ -18,8 +19,16 @@ export default {
         };
     },
     methods: {
+        onSubmit() {
+            this.addZipcode();
+            this.goToRestaurantDisplayView();
+
+        },
         addZipcode() {
             this.$store.commit('SET_ZIPCODE', this.zipcodeInput);
+        },
+        goToRestaurantDisplayView() {
+            this.$router.push({name:'restaurant-display', params: {zipCode: this.zipcodeInput}});
         }
     }
 };
