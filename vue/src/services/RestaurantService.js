7c86b464f0e0a65d455a48359656a59767c570cf
Jonathan Bangham <jdbangham@gmail.com>
2023-12-08 09:57:48 -0500

    Hours display now

--- vue/src/services/RestaurantService.js
+++ vue/src/services/RestaurantService.js
@@ -16,24 +16,10 @@ export default{
         let url = `/api/restaurants/search/${zipCode}`;
         console.log(url);
         return baseUrl.get(`/api/restaurants/search/${zipCode}`)
-<<<<<<< HEAD
-    },
-
-
-
-    getBusinessDetails(restaurantId) {
-        return baseUrl.get(`/api/businesses/get-details/${restaurantId}`)
-            .then(response => response.data)
-            .catch(error => {
-                console.error("Error fetching restaurant details:", error);
-                throw error;
-            });
-=======
     }, 
 
     getRestaurantDetails(restaurantId){
         return baseUrl.get(`/api/restaurants/get-details/${restaurantId}`)
->>>>>>> 3678e33f68cd7ea6c23e4c2277e80464639117cf
     }
 
 
